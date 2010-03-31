7fe9acbef5b38ca339b5c68a1528b8dfc5b09693
blatyo <blatyo@gmail.com>
2010-03-31 13:56:06 -0400

    Added user and authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resource :user_session
+  map.resource :account, :controller => "users"
+  map.resources :users
+
+  map.root :controller => "user_sessions", :action => "new"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -38,6 +43,6 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #map.connect ':controller/:action/:id'
+  #map.connect ':controller/:action/:id.:format'
 end
