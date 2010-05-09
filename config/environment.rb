915ba31a0bab1d329d3bf23e788481531472211c
blatyo <blatyo@gmail.com>
2010-05-09 09:36:32 -0400

    Added dm-charmer gem

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "mysql"
   config.gem "authlogic"
+  config.gem "db-charmer", :lib => "db_charmer"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
